ff1f779dc187a9374d031770054b3807b52bc48f
Brian S Paskin <21156194+bpaskin@users.noreply.github.com>
2021-08-03 09:08:56 -0400

    Added return code checking

--- BastionInstallCP4I/installcp4i.sh
+++ BastionInstallCP4I/installcp4i.sh
@@ -4,6 +4,7 @@
 NAMESPACE=cp4i
 ENTTITLEMENT_KEY=
 LOCAL_DOCKER_REGISTRY_HOST=
+LOCAL_DOCKER_REGISTRY_PORT=443
 LOCAL_DOCKER_USER=
 LOCAL_DOCKER_PASSWORD=
 CASE_VERSION=2.3.0
@@ -18,7 +19,6 @@ CASE_LOCAL_PATH=$OFFLINEDIR/$CASE_ARCHIVE
 CASE_INVENTORY_SETUP=operator
 OFFLINEDIR_ARCHIVE=offline.tgz
 CASE_LOCAL_PATH=$OFFLINEDIR/$CASE_ARCHIVE
-LOCAL_DOCKER_REGISTRY_PORT=443
 LOCAL_DOCKER_REGISTRY=$LOCAL_DOCKER_REGISTRY_HOST:$LOCAL_DOCKER_REGISTRY_PORT
 
 # check if the oc login command has been issued
@@ -37,22 +37,58 @@ oc project $NAMESPACE
 # save the data
 cloudctl case save --repo $CASE_REPO_PATH --case $CASE_NAME --version $CASE_VERSION --outputdir $OFFLINEDIR
 
+if [ $? -ne 0 ];
+then
+   echo "Failed saving - exiting"
+   exit 1
+fi
+
 # Store credentials 
 cloudctl case launch --case $HOME/offline/$CASE_ARCHIVE --inventory $CASE_INVENTORY_SETUP --action configure-creds-airgap --args "--registry $LOCAL_DOCKER_REGISTRY_HOST --user $LOCAL_DOCKER_USER --pass $LOCAL_DOCKER_PASSWORD"
 
+if [ $? -ne 0 ];
+then
+   echo "Failed storing credentials - exiting"
+   exit 1
+fi
+
 cloudctl case launch --case $HOME/offline/$CASE_ARCHIVE --inventory $CASE_INVENTORY_SETUP --action configure-creds-airgap --args "--registry cp.icr.io --user cp --pass $ENTTITLEMENT_KEY"
+
+if [ $? -ne 0 ];
+then
+   echo "Failed storing credentials - exiting"
+   exit 1
+fi
 
 # mirror the image locally
 cloudctl case launch --case $CASE_LOCAL_PATH --inventory $CASE_INVENTORY_SETUP --action mirror-images --args "--registry $LOCAL_DOCKER_REGISTRY --inputDir $OFFLINEDIR"
 
+if [ $? -ne 0 ];
+then
+   echo "Failed mirroring images - exiting"
+   exit 1
+fi
+
 # configure global image pull secret and ImageContentSourcePolicy
 cloudctl case launch --case $HOME/offline/$CASE_ARCHIVE --inventory $CASE_INVENTORY_SETUP --action configure-cluster-airgap --namespace $NAMESPACE --args "--registry $LOCAL_DOCKER_REGISTRY --user $LOCAL_DOCKER_USER --pass $LOCAL_DOCKER_PASSWORD --inputDir $OFFLINEDIR"
+
+if [ $? -ne 0 ];
+then
+   echo "Failed setting image content - exiting"
+   exit 1
+fi
 
 # verify above step
 oc get imageContentSourcePolicy
 
 # create CatalogSource
 cloudctl case launch --case $HOME/offline/${CASE_ARCHIVE} --inventory ${CASE_INVENTORY_SETUP} --action install-catalog --namespace ${NAMESPACE} --args "--registry ${LOCAL_DOCKER_REGISTRY} --inputDir $HOME/offline --recursive"
+
+if [ $? -ne 0 ];
+then
+   echo "Failed setting CatalogSource- exiting"
+   exit 1
+fi
 
 # verify the CatalogSource
 oc get pods -n openshift-marketplace
